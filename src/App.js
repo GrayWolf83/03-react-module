7f4b85ae92fd8c6d8a1bb8195f058c52d83889c4
Сергей Бернякович <sergejbernakovic@MacBook-Air-Sergej.local>
2023-03-11 21:34:06 +0500

    task/3 useHover

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
-import { useLocalStorage } from './hooks/useLocalStorage'
+import { useHover } from './hooks/useHover'
 import './App.css'
 
 function App() {
-	const [token, { setItem, removeItem }] = useLocalStorage('token')
+	const { hovered, ref } = useHover()
 
 	return (
 		<div>
-			<h4>Твой токен: {token}</h4>
-
-			<button onClick={() => setItem('token')}>Задать токен</button>
-			<button onClick={removeItem}>Удалить токен</button>
+			<h4 ref={ref} className='title'>
+				{hovered ? 'На меня навели мышку' : 'Наведи мышкой на меня'}
+			</h4>
 		</div>
 	)
 }
